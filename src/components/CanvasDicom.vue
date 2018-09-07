f5da542b5a0867fd5ca17a50d33c672a56d95205
Wilson <bianliuzhu@gmail.com>
2018-09-07 17:53:15 +0800

    增加 ElementUI

--- src/components/CanvasDicom.vue
+++ src/components/CanvasDicom.vue
@@ -1,13 +1,28 @@
 <template>
-  <div id="dicomImage">
-  </div>
+<el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main id="dicomImage"></el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  data() {
-    return {};
+  data: () => ({
+    show2: true
+  }),
+  mounted() {
+    const _this = this;
+    var element = document.getElementById("dicomImage");
+    console.log("cornerstone:", cornerstone);
+    console.log("dicomParser:", dicomParser);
+    console.log("cornerstoneWADOImageLoader:", cornerstoneWADOImageLoader);
+    cornerstone.enable(element);
+    let url = "wadouri:" + "http://127.0.0.1/testDICOM/CTStudy/1.5191KB.DCM";
+    this.loadAndViewImage(url);
   },
   methods: {
     //当点击加载图像时 调用 loadAndViewImage 加载 Dicom 图像
@@ -25,16 +40,6 @@ export default {
         }
       );
     }
-  },
-  mounted() {
-    const _this = this;
-    var element = document.getElementById("dicomImage");
-    console.log("cornerstone:", cornerstone);
-    console.log("dicomParser:", dicomParser);
-    console.log("cornerstoneWADOImageLoader:", cornerstoneWADOImageLoader);
-    cornerstone.enable(element);
-    let url = "wadouri:" + "http://127.0.0.1/testDICOM/CTStudy/1.5191KB.DCM";
-    this.loadAndViewImage(url);
   }
 };
 </script>
@@ -42,6 +47,36 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #dicomImage {
-  height: 96vh;
+  height: 88.5vh;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  padding: 0px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
